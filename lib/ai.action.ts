c1c37e97d995cb463c93cfa5d215d2e096903686
iftekharalamfahim <169484127+iftekharalamfahim@users.noreply.github.com>
2026-03-12 11:44:31 +0800

    feat: generate 2D to 3D design

---
+++ lib/ai.action.ts
@@ -0,0 +1,55 @@
+import puter from "@heyputer/puter.js";
+import {KOMPASCALE_RENDER_PROMPT} from "./constants";
+
+export async function fetchAsDataUrl(url: string): Promise<string> {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch image: ${response.statusText}`);
+  }
+
+  const blob = await response.blob();
+
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      if (typeof reader.result === "string") {
+        resolve(reader.result);
+      } else {
+        reject(new Error("Failed to convert blob to data URL"));
+      }
+    };
+    reader.onerror = () => {
+      reject(new Error("FileReader error"));
+    };
+    reader.readAsDataURL(blob);
+  });
+}
+
+export const generate3DView = async ({sourceImage}: Generate3DViewParams) => {
+  const dataUrl = sourceImage.startsWith('data')
+  ? sourceImage
+      : await fetchAsDataUrl(sourceImage);
+
+  const  base64Data = dataUrl.split(',')[1];
+  const mimeType = dataUrl.split(';')[0].split(':')[1];
+
+  if(!mimeType || !base64Data) throw new Error('Invalid source image  payload');
+
+  const response = await puter.ai.txt2img( KOMPASCALE_RENDER_PROMPT,{
+    provider: 'google',
+    model: 'gemini-3-pro-image-preview',
+    input_image: base64Data,
+    input_image_mime_type: mimeType,
+    ratio: {w:1024, h: 1024}
+  })
+
+  const rawImageUrl = (response as HTMLImageElement).src ?? null;
+
+  if(!rawImageUrl) return {renderedImage: null, renderedPath: undefined};
+
+  const renderedImage = rawImageUrl.startsWith('data')
+  ? rawImageUrl : await fetchAsDataUrl(rawImageUrl);
+
+  return {renderedImage, renderedPath: undefined};
+}
